83b45042014f643c3ede748e5e0e7e654f853d4d
elykozcc <100887630+elykozcc@users.noreply.github.com>
2022-03-18 09:44:27 +0800

    Update print_variable.c

--- print_variable.c
+++ print_variable.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
 
@@ -7,7 +7,7 @@ int main() {
         //give me some output messages in following format
         //and that can be done by add only ONE line code.
         //please refer to Stdio.h slides page 15.
-        
+
         //j=5, b=435
         //j=6, b=522
         //j=7, b=609
@@ -15,6 +15,6 @@ int main() {
         //j=9, b=783
         //...
         //j=22, b=1914
-        printf("i=%d, a=%d\n", i, a*j);
+        printf("j=%d, b=%d\n",j,b*j);
     }
 }
